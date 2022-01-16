f3baf7cf4f1e0d90f689b598f5e3dbdc9cc743c6
Sathyapriyan Kannan <sathyapriyankannan@gmail.com>
2022-01-17 00:15:35 +0530

    Add Reference Manual
    Add Reference Manual and it's tests

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import test_reference_manual
 from test_color_codes_identifier import *
+from reference_manual import *
 
 if __name__ == '__main__':
     execute_tests()
+    print_reference_manual()
+    test_reference_manual.execute_tests()
     print('Done :)')
